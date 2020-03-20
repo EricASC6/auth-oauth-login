2b9ff5757f000e5aed765e5bacd44fde0785d8f3
Eric Chen <ericchen9025@gmail.com>
2020-03-20 18:10:47 -0400

    markup + basic styles for login page

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
 const accountRoutes = require("./routes/account-routes");
 
 const app = express();
 
 // set up ejs
-app.set("view enginer", "ejs");
+app.set("view engine", "ejs");
+
+// static routes
+app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use("/acount", accountRoutes);
+app.use("/account", accountRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on port 3000"));

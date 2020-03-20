57ec722b5956fa476f25f1bd69571c44bc7de59c
Eric Chen <ericchen9025@gmail.com>
2020-03-20 16:29:51 -0400

    set up account-routes for login

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const accountRoutes = require("./routes/account-routes");
 
 const app = express();
 
 // set up ejs
 app.set("view enginer", "ejs");
+
+// routes
+app.use("/acount", accountRoutes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on port 3000"));

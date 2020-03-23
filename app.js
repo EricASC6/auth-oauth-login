542ec1dafcaca00bb7dad2e10bf5d16d84a88aa0
Eric Chen <ericchen9025@gmail.com>
2020-03-23 14:34:20 -0400

    set up connection to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+// Import environment variables
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Import dependencies
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const accountRoutes = require("./routes/account-routes");
 
 const app = express();
 
 // set up ejs
 app.set("view engine", "ejs");
+
+// Connect to mongodb
+const DB_URL = process.env.DB_URL;
+const db = mongoose.connection;
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+db.once("open", () => console.log(`connected to ${DB_URL}`));
+db.on("error", err => console.log(`connection error: ${err}`));
 
 // static routes
 app.use(express.static(path.join(__dirname, "public")));
